0bc96c86e701dedc7b920e232dfca197005948d5
Natan Rodrigues <natanrosilva@gmail.com>
2021-04-04 21:55:58 -0300

    Criando Operacao de Instanciacao

--- src/main/java/com/natan/cursomc/MccursoApplication.java
+++ src/main/java/com/natan/cursomc/MccursoApplication.java
@@ -1,13 +1,33 @@
 package com.natan.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class MccursoApplication {
+import com.natan.cursomc.domain.Categoria;
+import com.natan.cursomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class MccursoApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(MccursoApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null,"Informatica");
+		Categoria cat2 = new Categoria(null,"Escritorio");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		
+	}
+
+	
 }
